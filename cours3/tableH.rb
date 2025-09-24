4e0b955d719f18bb55d803d048ae7678698b1e4b
cuzimapanda2003 <marcblais03@gmail.com>
2025-09-24 08:54:50 -0400

    Create tableH.rb

---
+++ cours3/tableH.rb
@@ -0,0 +1,23 @@
+#pour run le programme: ruby /home/etd/EchangeDeDonne/cours3/tableH.rb
+
+voiture = {
+    couleur: "noir",
+    passager: 5,
+    marque: "hyundai",
+    vga: false,
+    km: 83502
+}
+voiture[:plaque] = 1234
+
+
+puts voiture[:couleur]
+puts voiture[:passager]
+puts voiture[:marque]
+puts voiture[:vga]
+puts voiture[:km]
+puts voiture[:plaque]
+
+puts voiture
+
+
+
